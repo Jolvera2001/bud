daf3d8942f1f1d41b94e97e199ab3a6836d3c7dd
Jolvera2001 <jolvera6@stedwards.edu>
2025-05-02 18:35:36 -0500

    starting with the transaction table

--- lib/viewmodels/transactions_viewmodel.dart
+++ lib/viewmodels/transactions_viewmodel.dart
@@ -1,11 +1,21 @@
+import 'package:bud/models/recurring_transaction.dart';
 import 'package:flutter/material.dart';
 
 class TransactionsViewmodel extends ChangeNotifier {
   String _name = "";
+  List<RecurringTransaction> _transactionList = [];
 
   String get name => _name;
+  List<RecurringTransaction> get transactionList => _transactionList;
+
+  void addTransaction(RecurringTransaction newItem) {
+    _transactionList.add(newItem);
+    notifyListeners();
+  }
+
   void updateName(String newName) {
     _name = newName;
     notifyListeners();
   }
+
 }
