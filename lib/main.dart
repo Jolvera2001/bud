72b9bd8bd90cde532846a5b4f8abcb054866b0e3
Jolvera2001 <jolvera6@stedwards.edu>
2025-05-02 17:53:31 -0500

    decomposed main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-import 'package:bud/screens/dashboard.dart';
-import 'package:bud/screens/transactions.dart';
+import 'package:bud/app.dart';
 import 'package:bud/viewmodels/transactions_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -26,60 +25,4 @@ class BudApp extends StatelessWidget {
   }
 }
 
-class MainApp extends StatefulWidget {
-  const MainApp({super.key});
 
-  @override
-  State<MainApp> createState() =>  _MainAppNavState();
-}
-
-class _MainAppNavState extends State<MainApp> {
-  int _selectedIndex = 0;
-  NavigationRailLabelType labelType = NavigationRailLabelType.all;
-
-  // screens
-  final List<Widget> _screens = const [
-    Dashboard(),
-    Transactions()
-  ];
-
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Row(
-          children: [
-            NavigationRail(
-              selectedIndex: _selectedIndex,
-              labelType: labelType,
-              onDestinationSelected: (int index) {
-                setState(() {
-                  _selectedIndex = index;
-                });
-              },
-              destinations: const <NavigationRailDestination> [
-                  NavigationRailDestination(
-                    icon: Icon(Icons.dashboard_outlined),
-                    selectedIcon: Icon(Icons.dashboard), 
-                    label: Text("Dashboard")
-                  ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.add_to_photos_outlined),
-                    selectedIcon: Icon(Icons.add_to_photos), 
-                    label: Text("Transactions")
-                  )
-              ]
-            ),
-            const VerticalDivider(thickness: 1, width: 1),
-
-            // main content
-            Expanded(
-              child: _screens[_selectedIndex],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
