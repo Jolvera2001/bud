4fb6f8b5fdf613ef54ab3fdfff3f58abaa4b8a62
Jolvera2001 <jolvera6@stedwards.edu>
2025-05-02 19:41:42 -0500

    alignment stuff

--- lib/components/generics/form_dialog.dart
+++ lib/components/generics/form_dialog.dart
@@ -11,19 +11,18 @@ Future<T?> showFormDialog<T>({
     barrierLabel: formLabel,
     barrierColor: Colors.black54,
     transitionDuration: Duration(milliseconds: 500),
-    pageBuilder:
-        (_, _, _) => Align(
-          alignment: Alignment.centerRight,
-          child: Material(
-            color: Colors.white,
-            elevation: 24,
-            child: SizedBox(
-              height: double.infinity,
-              width: MediaQuery.of(context).size.width * 0.3,
-              child: form, // TODO: handle this later for dynamic form providing thingy
-            ),
-          ),
+    pageBuilder: (_, _, _) => Align(
+      alignment: Alignment.centerRight,
+      child: Material(
+        color: Colors.white,
+        elevation: 24,
+        child: SizedBox(
+          height: double.infinity,
+          width: MediaQuery.of(context).size.width * 0.3,
+          child: form, // TODO: handle this later for dynamic form providing thingy
         ),
+      ),
+    ),
     transitionBuilder: (_, anim, _, child) {
       return SlideTransition(
         position: Tween(begin: Offset(1, 0), end: Offset(0, 0))
